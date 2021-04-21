2c3a79a3cdda718bded9e6280d689b6417cdce3d
gabrielevitali <79760224+gabrielevitali@users.noreply.github.com>
2021-04-21 17:31:05 +0200

    3rd commit

--- infoVoli.js
+++ infoVoli.js
@@ -1,6 +1,5 @@
-let isEmpty = 1;
 const API_KEY = '792898ddad7f8991f83eed9dfaaf9801';
-let dateValue = undefined;
+let dateValue = undefined; //variabile globale
 
 //const iata = 'CDG';
 //const url = "http://api.aviationstack.com/v1/flights?access_key=" + API_KEY + '&dep_iata=' + iata; 
@@ -10,29 +9,32 @@ let dateValue = undefined;
 function addFlight(flight){
     
     
+    
 }
 
 //funzione che gestisce il file .json restituito da aviationStack inseguito a richiesta
 function onJson (json){
-    if(isEmpty){
-        isEmpty--;
-    }
-    else {
-        const flightGrid = document.querySelector('#flightGrid');
-    }
-
-    const num = json.data.length;
-    //console.log('Numero di elementi restituiti: ' + num);
-    for(let i=0; i < num; i++){
-        doc = json.data[i];
-        //console.log(doc.flight_date + '\n');
-        if(doc.flight_date == dateValue){
-            console.log("Ho trovato una corrispondenza");
-            //aggiungo dinamicamente il volo per cui ho trovato una corrispondenza
-            addFlight(doc);
-        }
-    }
+    const flightGrid = document.querySelector('#flightGrid');
+    flightGrid.innerHTML = '';
     
+        const num = json.data.length;
+        console.log('Numero di elementi restituiti: ' + num);
+        for(let i=0; i < num; i++){
+            doc = json.data[i];
+            //console.log('Data: ' + doc.flight_date + '\n');
+            if(doc.flight_date === dateValue){
+                console.log('Ho trovato una corrispondenza in data' + dateValue);
+                //aggiungo dinamicamente il volo per cui ho trovato una corrispondenza
+                //flight_number = doc.flight.number;
+                console.log('Numero di volo n.' + i + ': ' + doc.flight.number + '\n');
+                console.log(doc.departure.airport + ' ' + doc.departure.iata + '\n');
+                console.log(doc.arrival.airport + ' ' + doc.arrival.iata + '\n');
+                console.log(doc.departure.scheduled + '\n'); //gestione data mediante apposita funzione
+                console.log(doc.arrival.scheduled + '\n'); //gestione data mediante apposita funzione
+                console.log(doc.flight_status + '\n');
+                //addFlight(doc);
+            }
+        }
     console.log(json);
 }
 
@@ -51,19 +53,25 @@ function search(event){
     const arrivalValue = encodeURIComponent(form.arrivalAirport.value);
     console.log(arrivalValue);
     dateValue = encodeURIComponent(form.date.value);
-    console.log(dateValue);
+    console.log('Data originale:' + dateValue);
+    dateValue = '2021-04-21';
+    console.log('Data modificata:' + dateValue);
 
     //Se aeroporto di partenza e aeroporto di arrivo coincidono...
     if(departureValue === arrivalValue){
         alert('Occorre scegliere Aeroporto di Partenza e Aeroporto di Arrivo diversi fra loro');
     }
+    /*
     //Se non è stata selezionata una data...
     if(dateValue === ''){
         alert('Occorre scegliere una data.');
     }
+    */
+   
     //Preparo richiesta...
     const url = "http://api.aviationstack.com/v1/flights?access_key=" + API_KEY + '&dep_iata=' + departureValue + '&arr_iata=' + arrivalValue; 
-    //const url = "http://api.aviationstack.com/v1/flights?access_key=" + API_KEY + '&dep_iata=' + departureValue + '&arr_iata=' + arrivalValue + '&flight_date=' + dateValue;
+    //const url = "http://api.aviationstack.com/v1/flights?access_key=" + API_KEY + '&dep_iata=' + departureValue + '&arr_iata=' + arrivalValue + '&flight_status=' + 'landed'; 
+    //const url = "http://api.aviationstack.com/v1/flights?access_key=" + API_KEY + '&flight_date=' + dateValue; 
     console.log(url);
     //Effettuo richiesta...
     fetch(url).then(onResponse).then(onJson);
