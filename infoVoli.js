11316969bda37600ac7106e358a1cebfcdb0738a
gabrielevitali <79760224+gabrielevitali@users.noreply.github.com>
2021-04-26 20:54:59 +0200

    6th Commit

--- infoVoli.js
+++ infoVoli.js
@@ -1,69 +1,23 @@
+//API_KEY ottenuta in seguito alla registrazione al servizio di AviationStack
 const API_KEY = '792898ddad7f8991f83eed9dfaaf9801';
+
+//credenziali ottenute in seguito alla registrazione al servizio di Amadeus.com
+const client_id = 'OlD4Z6GmBYXUpi23jSUPvCAfZQNniv0z';
+const client_secret = 'SGNujg2Wwa1XdeAA';
+
+//variabili globali
+let token;
 let dateValueOriginal = undefined; //variabile globale
 let dateValue = undefined; //variabile globale
-//let found = false; //variabile globale (flag)
+let found; //variabile globale (flag)
 
 //const iata = 'CDG';
 //const url = "http://api.aviationstack.com/v1/flights?access_key=" + API_KEY + '&dep_iata=' + iata; 
 //const url = "http://api.aviationstack.com/v1/flights?access_key=" + API_KEY; 
 
-//funzione che riceve un flight e lo aggiunge dinamicamente nella griglia dei voli, inizialmente nascosta
-/*
-function addFlight(doc){
-    const flightGrid = document.querySelector('#flightGrid');
-    console.log('Sono entrato in questa funzione')
-    const flight = document.createElement('div');
-    // const flightGridHeading = document.querySelector('.flightGridHeading').style = 'flex';
-    flight.classList.add('flightGridHeading');
-    flight.style.display = 'flex';
-    flightGrid.appendChild(flight);
-
-    
-    //creo p per N. Volo
-    const nVolo = document.createElement('p');
-    flight.classList.add('paragraph');
-    nVolo.textContent = doc.flight.number;
-    flight.appendChild(nVolo);
-
-    //creo p per Aeroporto di Partenza
-    const departureAirport = document.createElement('p');
-    flight.classList.add('paragraph');
-    departureAirport.textContent = doc.departure.airport + ' ' + doc.departure.iata;
-    flight.appendChild(departureAirport);
-
-    //creo p per Aeroporto di Arrivo
-    const arrivalAirport = document.createElement('p');
-    flight.classList.add('paragraph');
-    arrivalAirport.textContent = doc.arrival.airport + ' ' + doc.arrival.iata;
-    flight.appendChild(arrivalAirport);
-
-    //creo p per Data e Orario di Partenza
-    let departureDate = document.createElement('p');
-    flight.classList.add('paragraph');
-    //gestisco data e orario di Partenza
-    const depTime = doc.departure.scheduled.substring(11, 16);
-    departureDate.textContent = dateValueOriginal + ' ' + depTime;
-    flight.appendChild(departureDate);
-
-    //creo p per Data e Orario di Arrivo
-    let arrivalDate = document.createElement('p');
-    flight.classList.add('paragraph');
-    //gestisco data e orario di Partenza
-    const arrTime = doc.arrival.scheduled.substring(11, 16);
-    arrivalDate.textContent = dateValueOriginal + ' ' + arrTime;
-    flight.appendChild(arrivalDate);
-
-    //creo p per Stato Volo
-    let status = document.createElement('p');
-    flight.classList.add('paragraph');
-    status.textContent = doc.flight_status;
-    flight.appendChild(status);
-}
-*/
-
 //funzione che riceve un flight e lo aggiunge dinamicamente nella tabella dei voli, inizialmente nascosta
 function addFlight(doc){
-    console.log('La funzione addFlight() è stata chiamata.\n');
+    //console.log('La funzione addFlight() è stata chiamata.\n');
 
     /* Se tale funzione viene chiamata, significa che è stata trovata almeno una corrispondenza,
        dunque rendo visibile l'intestazione della tabella */
@@ -120,15 +74,29 @@ function addFlight(doc){
     return;
 }
 
+function onAmadeusJson(json){
+    const num = json.length;
+    console.log(num);
+    /*
+    for(let i=0; i < num; i++0){
+        doc = json.data[i]
+    }
+        console.log(doc.name); */
+}
+
+function onAmadeusResponse(response){
+    return response.json;
+}
 
 //funzione che gestisce il file .json restituito da aviationStack inseguito a richiesta
-function onJson (json){
+function onJson (json, found){
     const rows = document.querySelectorAll('#flightTable tr');
     for(row of rows){
         if(row.dataset.type !== 'heading'){
             row.innerHTML = '';
         }
     }
+    found = false;
     
     const num = json.data.length;
     //console.log('Numero di elementi restituiti: ' + num);
@@ -137,32 +105,49 @@ function onJson (json){
         //console.log('Data: ' + doc.flight_date + '\n');
         if(doc.flight_date === dateValue){
             //console.log('Ho trovato una corrispondenza in data' + dateValue);
-            //found = true; //setto la variabile flag a true
-            //aggiungo dinamicamente il volo per cui ho trovato una corrispondenza
+            found = true; //setto la variabile flag a true
+            //console.log('[onJson function] found = ' + found);
+            //stampa di controllo, su console, dell'elemento per cui è stata trovata una corrispondenza
+            /*
             console.log('Numero di volo n.' + i + ': ' + doc.flight.number + '\n');
             console.log(doc.departure.airport + ' ' + doc.departure.iata + '\n');
             console.log(doc.arrival.airport + ' ' + doc.arrival.iata + '\n');
             console.log(doc.departure.scheduled + '\n');
             console.log(doc.arrival.scheduled + '\n');
             console.log(doc.flight_status + '\n');
+            */
+            //aggiungo dinamicamente il volo per cui ho trovato una corrispondenza
             addFlight(doc); //chiamata alla funzione che gestisce l'inserimento di una nuova riga (volo) in tabella
+
         }
     }
     console.log(json);
 
-    /*
+    //console.log('[fuori dal ciclo for() ] found = ' + found);
     if(found === false){
         alert('Nessun volo trovato per i parametri di ricerca');
+        const heading = document.querySelector('#heading');
+        heading.style.display = 'none';
     }
-    */
+    else{
+            if(found === true){
+                //effettuo richiesta ad Amadeus.com /v1/reference-data/locations/pois
+                fetch('https://test.api.amadeus.com/v1/reference-data/locations/pois?latitude=41.397158&longitude=2.160873', {
+                    headers: {
+                        'Authorization': 'Bearer ' + token
+                    }
+                }).then(onAmadeusResponse).then(onAmadeusJson);
+            }
+    }
+
+    found = false;
 }
 
 function onResponse (response){
-    console.log(response.status);
+    //console.log(response.status);
     return response.json();
 }
 
-//fetch(url).then(onResponse).then(onJson);
 
 function search(event){
     event.preventDefault();
@@ -172,8 +157,7 @@ function search(event){
     const arrivalValue = encodeURIComponent(form.arrivalAirport.value);
     //console.log(arrivalValue);
     dateValueOriginal = encodeURIComponent(form.date.value);
-    console.log('Data originale:' + dateValue);
-    dateValue = '2021-04-21';
+    //console.log('Data originale:' + dateValueOriginal + '\n');
 
     let data = new Date();
     let day= data.getDate();
@@ -183,13 +167,17 @@ function search(event){
     }
     let year = data.getFullYear();
     dateValue = year + '-' + month + '-' + day;
-    console.log('Data modificata (dateValue):' + dateValue);
+    //console.log('Data modificata (dateValue):' + dateValue + '\n');
 
     //Se aeroporto di partenza e aeroporto di arrivo coincidono...
     if(departureValue === arrivalValue){
         alert('Occorre scegliere Aeroporto di Partenza e Aeroporto di Arrivo diversi fra loro');
     }
     
+    /* Al fine di verificare che l'utente selezioni una data nel form di ricerca,
+       non effettuo il controllo che segue, bensì in HTML specifico che
+       l'elemento input  di tipo "date" è required */
+
     /*
     //Se non è stata selezionata una data...
     if(dateValue === ''){
@@ -199,12 +187,32 @@ function search(event){
    
     //Preparo richiesta...
     const url = "http://api.aviationstack.com/v1/flights?access_key=" + API_KEY + '&dep_iata=' + departureValue + '&arr_iata=' + arrivalValue; 
-    //const url = "http://api.aviationstack.com/v1/flights?access_key=" + API_KEY + '&dep_iata=' + departureValue + '&arr_iata=' + arrivalValue + '&flight_status=' + 'landed'; 
-    //const url = "http://api.aviationstack.com/v1/flights?access_key=" + API_KEY + '&flight_date=' + dateValue; 
-    console.log(url);
+    //console.log(url);
     //Effettuo richiesta...
-    fetch(url).then(onResponse).then(onJson);
+    fetch(url).then(onResponse).then(onJson, found);
 }
 
+
+function onTokenJson(json){
+    token = json.access_token; //memorizzo il token ricevuto da Amadeus.com
+    console.log(json.application_name + '\n');
+    console.log(json.access_token);
+}
+
+function onTokenResponse(response){
+    return response.json();
+}
+
+//Richiesta del token al servizio di Amadeus.com
+fetch('https://test.api.amadeus.com/v1/security/oauth2/token', {
+    method: 'post',
+    body: 'grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret,
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+}).then(onTokenResponse).then(onTokenJson);
+
+//seleziono il form di ricerca e gli associo l'handler search()
 const form = document.forms['searchFlight'];
 form.addEventListener('submit', search);
+
